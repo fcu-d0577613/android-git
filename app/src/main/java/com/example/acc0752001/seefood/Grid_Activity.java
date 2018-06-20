6f116d23d175136e2857c73f4d04142763e93d6a
caa93215 <caa93215@gmail.com>
2018-06-21 00:47:03 +0800

    版本

--- app/src/main/java/com/example/acc0752001/seefood/Grid_Activity.java
+++ app/src/main/java/com/example/acc0752001/seefood/Grid_Activity.java
@@ -7,14 +7,13 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.GridView;
 import android.widget.ImageView;
 
-public class Grid_Activity extends AppCompatActivity {
+public class Grid_Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
